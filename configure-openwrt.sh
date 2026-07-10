684282fb17fddee3afc76255484063bbb5602f7b
Matteo Lutz <matteo.lutz06@outlook.de>
2026-07-10 11:36:15 +0200

    feat: configure script for openwrt

---
+++ configure-openwrt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -p "This will configure the current directory to be a valid OpenWRT package. Please move the repository root to the \"packages/\" in the OpenWRT SDK (y/N) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Aborting..."
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+# Cleaning
+make clean
+
+# Make src directory
+mkdir src
+
+# Move all sources and headers to src
+mv *.c src/
+mv *.h src/
+
+# Move include/ and lib/ dirs to src
+mv include src/
+mv lib src/
+
+# Move Makefile to src
+mv Makefile src/
+
+# Rename the openwrt Makefile
+mv Makefile.openwrt Makefile
